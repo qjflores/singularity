bf44abccea085c8145cc8d0dcc22801120a82857
Arseniy Klempner <adklempner@gmail.com>
2017-03-06 16:51:59 +0530

    Rewrite tests to use async/await

--- test/internet.js
+++ test/internet.js
@@ -3,14 +3,9 @@ var Internet = artifacts.require("./Internet.sol");
 contract("Internet", function(accounts){
   var internet;
   var _owner = accounts[0];
-  it("internet-init", function(){
-    return Internet.new("Singularity Internet", "Internet Provider for the Singularity",{from:_owner})
-      .then(function(internetContract){
-        if(internetContract.address){
-          internet = internetContract;
-        } else {
-          throw new Error("no contract address")
-        }
-      })
+  it("internet-init", async function(){
+    internet = await Internet.new("Singularity Internet", "Internet Provider for the Singularity",{from:_owner});
+    if(!internet.address)
+      throw new Error("no contract address")
   })
 })
--- test/user.js
+++ test/user.js
@@ -5,63 +5,31 @@ contract("User", function(accounts){
   var user;
   var provider;
   var balance = (acct) => {return web3.fromWei(web3.eth.getBalance(acct), 'ether').toNumber() }
-  it("init", function(){
-    return User.new("myUserName", {from:accounts[1]})
-      .then(function(userContract){
-        if(userContract.address){
-          user = userContract;
-        } else {
-          throw new Error("no contract address");
-        }
-        return true;
-      })
-      .then(function(value){
-        return user.userName.call()
-      })
-      .then(function(userName){
-        assert.equal(userName, "myUserName");
-      })
+  it("init", async function(){
+    user = await User.new("myUserName", {from:accounts[1]});
+    if(!user.address)
+      throw new Error("no contract address");
+    let userName = await user.userName();
+    assert.equal(userName, "myUserName");
   })
-  it("register-with-a-service-provider-then-set-debt-then-pay-debt",function(){
-    return Provider.new("provider1", "provider of service 1")
-      .then(function(providerContract){
-        if(providerContract.address){
-          provider = providerContract;
-        } else {
-          throw new Error("no contract address");
-        }
-        return true;
-      })
-      .then(function(value){
-        assert.equal(0, web3.toWei(balance(provider.address),"ether"));
-        return user.registerToProvider(provider.address);
-      })
-      .then(function(txHash){
-        return user.services(provider.address)
-      })
-      .then(function(serviceInfo){
-        assert.equal(serviceInfo[0], true);
-        // TODO use the block number instead of time for last updated
-        //assert.equal(serviceInfo[1].c[0], 1486464963);
-        assert.equal(serviceInfo[2].c[0], 0);
-        return provider.setDebt(web3.toWei(10,"ether"), user.address)
-      })
-      .then(function(txHash){
-        return user.services(provider.address)
-      })
-      .then(function(serviceInfo){
-        assert.equal(serviceInfo[2].c[0], 100000);
-        return provider.recievePayment(user.address,{from:accounts[1], value:serviceInfo[2].c[0]})
-      })
-      .then(function(txHash){
-        return user.services(provider.address);
-      })
-      .then(function(serviceInfo){
-        assert.equal(serviceInfo[2].c[0], 0);
-        return true;
-      })
-      .then(function(value){
-        assert.equal(100000,web3.toWei(balance(provider.address),"ether"));
-      })
+  it("register-with-a-service-provider-then-set-debt-then-pay-debt", async function(){
+    provider = await Provider.new("provider1", "provider of service 1");
+    if(!provider.address)
+      throw new Error("no contract address");
+
+    assert.equal(0, web3.toWei(balance(provider.address),"ether"));
+    await user.registerToProvider(provider.address);
+    let serviceInfo = await user.services(provider.address);
+    assert.equal(serviceInfo[0], true);
+    // TODO use the block number instead of time for last updated
+    //assert.equal(serviceInfo[1].c[0], 1486464963);
+    assert.equal(serviceInfo[2].c[0], 0);
+    await provider.setDebt(web3.toWei(10,"ether"), user.address);
+    serviceInfo = await user.services(provider.address);
+    assert.equal(serviceInfo[2].c[0], 100000);
+    await provider.recievePayment(user.address,{from:accounts[1], value:serviceInfo[2].c[0]});
+    serviceInfo = await user.services(provider.address);
+    assert.equal(serviceInfo[2].c[0], 0);
+    assert.equal(100000,web3.toWei(balance(provider.address),"ether"));
   })
 })
